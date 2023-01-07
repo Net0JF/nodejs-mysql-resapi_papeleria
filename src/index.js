5d4790b0b0b7b63d54b418c86ec6fb08b68e17a9
Ernesto <jimenezfloresernesto@gmail.com>
2023-01-07 01:48:34 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import empleadosRoutes from './routes/empleados.routes.js'
+import indexRoutes from './routes/index.routes.js'
+
+const app = express()
+
+
+app.use(indexRoutes)
+app.use(empleadosRoutes)
+
+
+app.listen(3000)
+console.log('se escucha puerto 3000')
